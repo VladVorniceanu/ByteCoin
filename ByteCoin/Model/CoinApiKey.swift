4823240ac3c7a9536f3f9a67566d4a92c8f00b27
VladVorniceanu <vorniceanu.v@gmail.com>
2024-02-26 12:43:04 +0200

    [main] Basic fetch mechanism done

---
+++ ByteCoin/Model/CoinApiKey.swift
@@ -0,0 +1,9 @@
+//
+//  CoinApiKey.swift
+//  ByteCoin
+//
+//  Created by Vlad Vorniceanu on 26.02.2024.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ ByteCoin/Model/CoinStatus.swift
@@ -0,0 +1,9 @@
+//
+//  CoinStatus.swift
+//  ByteCoin
+//
+//  Created by Vlad Vorniceanu on 26.02.2024.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
